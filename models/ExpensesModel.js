c610cbee7eb62232f1227ca550e8c1e1fd268773
Fabian <128451994+fabs-pe@users.noreply.github.com>
2025-08-19 16:10:31 +1000

    wip: expenses model

--- models/ExpensesModel.js
+++ models/ExpensesModel.js
@@ -5,26 +5,46 @@ const Schema = mongoose.Schema;
 const ExpenseSchema = new Schema({
 
     user:{
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
 
     },
 
     category:{
+        type:mongoose.Schemma.ObjectId,
+        ref: 'Category',
+        required: true
 
     },
     
     expenseName:{
-
+        type: String,
+        required: true,
+        unquie: false,
+        maxLength: 80
+    
     },
 
     description:{
+        type: String,
+        required: true,
+        unquie: false,
+        maxLength: 255
 
     },
 
     amount:{
+        type: Number,
+        required: true,
+        unquie: false
 
     },
 
     date:{
+        type: Date,
+        required: true,
+        unquie: false
 
     },
 
