54d38e536deaed5227ec37bc682ff24bc643fdd3
Fabian <128451994+fabs-pe@users.noreply.github.com>
2025-09-02 12:42:32 +1000

    wip: user routes

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -5,18 +5,21 @@ const router = express.Router();
 
 
 // create new user
-router.post("/register", async (request, reponse) =>{
+router.post("/register", async (request, response) =>{
     try{
         let newUser = await User.create(request.body)
-        reponse.status(201).json({message: 'User registered successfully!'});
+        response.status(201).json({message: 'User registered successfully!'});
 
-        reponse.json({user: newUser});
+        response.json({user: newUser});
     } catch{
-        reponse.status(500).json({message: "An error occured during the update"});
+        response.status(500).json({message: "An error occured during the update"});
 
     }
 });
 
 // POST to users/login
 
-router.post
+router.post("login", async (request, response))
+
+
+module.exports = router;
