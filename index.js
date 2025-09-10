a70d3ec745ae1ab3e6d92f5383f8a9d2ebc036c4
Fabian <128451994+fabs-pe@users.noreply.github.com>
2025-09-11 09:55:57 +1000

    wip- files

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
-const app = require('./server');
+const app = require('./src/server');
 
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
